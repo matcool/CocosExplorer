b329f05910e48c4d9d27c553201b37dcd9e64649
matcool <26722564+matcool@users.noreply.github.com>
2021-03-13 12:38:36 -0300

    new thread func method and add node (wip)

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,7 +2,8 @@
 #include <imgui.h>
 #include "opengl-imgui-hook/imgui_hook.h"
 #include <minhook/include/MinHook.h>
-
+#include <queue>
+#include <mutex>
 
 #define _NODE_NAME(type) if (dynamic_cast<type*>(node)) return #type"";
 const char* getNodeName(CCNode* node) {
@@ -21,20 +22,14 @@ const char* getNodeName(CCNode* node) {
     _NODE_NAME(CCLayer);
     _NODE_NAME(CCSprite);
     _NODE_NAME(CCScene);
+    _NODE_NAME(CCNodeRGBA);
     return "CCNode";
 }
 
-void* schData;
-char schData2[256];
-class scheduleFunctions {
-public:
-    void labelSetString(float dt) {
-        auto label = static_cast<CCLabelProtocol*>(schData);
-        label->setString(schData2);
-    }
-};
+std::queue<std::function<void()>> threadFunctions;
+std::mutex threadFunctionsMutex;
 
-void generateTree(CCNode* node, int i = 0) {
+void generateTree(CCNode* node, unsigned int i = 0) {
     //                                                ew
     if (ImGui::TreeNode(node, node->getTag() == -1 ? "[%d] %s" : "[%d] %s (%d)", i, getNodeName(node), node->getTag())) {
         if (ImGui::TreeNode(node + 1, "Attributes")) {
@@ -44,7 +39,91 @@ void generateTree(CCNode* node, int i = 0) {
                 ImGui::TreePop();
                 return;
             }
+            ImGui::SameLine();
+            if (ImGui::Button("Add Child")) {
+                ImGui::OpenPopup("Add Child");
+            }
 
+            if (ImGui::BeginPopupModal("Add Child")) {
+                static int item = 0;
+                ImGui::Combo("Node", &item, "CCNode\0CCLabelBMFont\0CCLabelTTF\0CCSprite\0CCMenuItemSpriteExtra\0");
+
+                static int tag = -1;
+                ImGui::InputInt("Tag", &tag);
+
+                static char text[256];
+                static char labelFont[256] = "bigFont.fnt";
+                if (item == 1) {
+                    ImGui::InputText("Text", text, 256);
+                    ImGui::InputText("Font", labelFont, 256);
+                }
+                static int fontSize = 20;
+                if (item == 2) {
+                    ImGui::InputText("Text", text, 256);
+                    ImGui::InputInt("Font Size", &fontSize);
+                }
+                static bool frame = false;
+                if (item == 3 || item == 4) {
+                    ImGui::InputText("Texture", text, 256);
+                    ImGui::Checkbox("Frame", &frame);
+                }
+
+                ImGui::Separator();
+
+                if (ImGui::Button("Add")) {
+                    threadFunctionsMutex.lock();
+                    threadFunctions.push([node] {
+                        CCNode* _child = nullptr;
+                        switch (item) {
+                        case 0:
+                            _child = CCNode::create();
+                            break;
+                        case 1: {
+                            auto child = CCLabelBMFont::create(text, labelFont);
+                            _child = child;
+                            break;
+                        }
+                        case 2: {
+                            auto child = CCLabelTTF::create(text, "Arial", fontSize);
+                            _child = child;
+                            break;
+                        }
+                        case 3: {
+                            CCSprite* child;
+                            if (frame)
+                                child = CCSprite::createWithSpriteFrameName(text);
+                            else
+                                child = CCSprite::create(text);
+                            _child = child;
+                            break;
+                        }
+                        case 4: {
+                            CCSprite* sprite;
+                            if (frame)
+                                sprite = CCSprite::createWithSpriteFrameName(text);
+                            else
+                                sprite = CCSprite::create(text);
+                            _child = CCMenuItemSpriteExtra::create(sprite, sprite, nullptr, nullptr);
+                        }
+                        default:
+                            return;
+                        }
+                        if (_child) {
+                            _child->setTag(tag);
+                            node->addChild(_child);
+                        }
+                    });
+                    threadFunctionsMutex.unlock();
+
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::SameLine();
+                if (ImGui::Button("Cancel")) {
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+            ImGui::Text("Addr: 0x%p", node);
             if (node->getUserData()) {
                 ImGui::Text("User data: 0x%p", node->getUserData());
             }
@@ -98,13 +177,16 @@ void generateTree(CCNode* node, int i = 0) {
             if (dynamic_cast<CCLabelProtocol*>(node) != nullptr) {
                 auto labelNode = dynamic_cast<CCLabelProtocol*>(node);
                 auto labelStr = labelNode->getString();
-                strcpy_s(schData2, labelStr);
-                ImGui::InputText("Text", schData2, 256);
-                if (strcmp(schData2, labelStr)) {
-                    // there has to be a better way of passing data to the scheduled function
-                    // maybe find another way of running a function in the gd thread
-                    schData = labelNode;
-                    CCDirector::sharedDirector()->getRunningScene()->scheduleOnce(schedule_selector(scheduleFunctions::labelSetString), 0);
+                char text[256];
+                strcpy_s(text, labelStr);
+                ImGui::InputText("Text", text, 256);
+                if (strcmp(text, labelStr)) {
+                    std::string _text(text);
+                    threadFunctionsMutex.lock();
+                    threadFunctions.push([labelNode, _text]() {
+                        labelNode->setString(_text.c_str());
+                    });
+                    threadFunctionsMutex.unlock();
                 }
             }
 
@@ -112,7 +194,7 @@ void generateTree(CCNode* node, int i = 0) {
         }
         
         auto children = node->getChildren();
-        for (int i = 0; i < node->getChildrenCount(); ++i) {
+        for (unsigned int i = 0; i < node->getChildrenCount(); ++i) {
             auto child = children->objectAtIndex(i);
             generateTree(dynamic_cast<CCNode*>(child), i);
         }
@@ -121,13 +203,17 @@ void generateTree(CCNode* node, int i = 0) {
 }
 
 void RenderMain() {
-    ImGui::Begin("cocos2d explorer");
-    auto director = cocos2d::CCDirector::sharedDirector();
-    // thank u andre
-    const bool enableTouch = !ImGui::GetIO().WantCaptureMouse;
-    director->getTouchDispatcher()->setDispatchEvents(enableTouch);
-    auto curScene = director->getRunningScene();
-    generateTree(curScene);
+    auto& style = ImGui::GetStyle();
+    style.ColorButtonPosition = ImGuiDir_Left;
+    // ImGui::ShowDemoWindow();
+    if (ImGui::Begin("cocos2d explorer"), nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_AlwaysHorizontalScrollbar) {
+        auto director = cocos2d::CCDirector::sharedDirector();
+        // thank u andre
+        const bool enableTouch = !ImGui::GetIO().WantCaptureMouse;
+        director->getTouchDispatcher()->setDispatchEvents(enableTouch);
+        auto curScene = director->getRunningScene();
+        generateTree(curScene);
+    }
     ImGui::End();
 }
 
@@ -137,12 +223,34 @@ void __fastcall dispatchKeyboardMSGHook(void* self, void*, int key, bool down) {
     dispatchKeyboardMSG(self, key, down);
 }
 
+inline void(__thiscall* schUpdate)(CCScheduler* self, float dt);
+void __fastcall schUpdateHook(CCScheduler* self, void*, float dt) {
+    /*while (!threadActions.empty()) {
+        auto action = threadActions.back();
+        if (action.type == ThreadActionType::LABEL_SET_STRING) {
+            auto data = action.data.labelSetString;
+            data.node->setString(data.string);
+        }
+        threadActions.pop();
+    }*/
+    threadFunctionsMutex.lock();
+    while (!threadFunctions.empty()) {
+        threadFunctions.back()();
+        threadFunctions.pop();
+    }
+    threadFunctionsMutex.unlock();
+    return schUpdate(self, dt);
+}
+
 DWORD WINAPI my_thread(void* hModule) {
     ImGuiHook::Main(hModule);
     auto cocosBase = GetModuleHandleA("libcocos2d.dll");
     auto dispatchAddr = GetProcAddress(cocosBase, "?dispatchKeyboardMSG@CCKeyboardDispatcher@cocos2d@@QAE_NW4enumKeyCodes@2@_N@Z");
     MH_CreateHook(dispatchAddr, &dispatchKeyboardMSGHook, reinterpret_cast<void**>(&dispatchKeyboardMSG));
     MH_EnableHook(dispatchAddr);
+    auto schUpdateAddr = GetProcAddress(cocosBase, "?update@CCScheduler@cocos2d@@UAEXM@Z");
+    MH_CreateHook(schUpdateAddr, &schUpdateHook, reinterpret_cast<void**>(&schUpdate));
+    MH_EnableHook(schUpdateAddr);
     return 0;
 }
 
