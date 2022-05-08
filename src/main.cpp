b0b0934576b4443e29cab40aa3a455edc7ed81d6
matcool <26722564+matcool@users.noreply.github.com>
2022-05-07 22:50:38 -0300

    switch to two panel design and rewrite a lot of the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <Windows.h>
 #include <cocos2d.h>
 #include <imgui.h>
 #include <MinHook.h>
-#include <queue>
-#include <mutex>
 #include <fstream>
 #include <sstream>
 #include <imgui-hook.hpp>
+#include <string_view>
+#include <imgui/misc/cpp/imgui_stdlib.h>
 
 using namespace cocos2d;
 
@@ -15,12 +15,36 @@ const char* getNodeName(CCNode* node) {
     return typeid(*node).name() + 6;
 }
 
-void clipboardText(const std::string& text) {
+#define public_cast(value, member) [](auto* v) { \
+    class FriendClass__; \
+    using T = std::remove_pointer<decltype(v)>::type; \
+    class FriendeeClass__: public T { \
+    protected: \
+        friend FriendClass__; \
+    }; \
+    class FriendClass__ { \
+    public: \
+        auto& get(FriendeeClass__* v) { return v->member; } \
+    } c; \
+    return c.get(reinterpret_cast<FriendeeClass__*>(v)); \
+}(value)
+
+template <typename T, typename U>
+T union_cast(U value) {
+    union {
+        T a;
+        U b;
+    } u;
+    u.b = value;
+    return u.a;
+}
+
+void setClipboardText(std::string_view text) {
     if (!OpenClipboard(NULL)) return;
     if (!EmptyClipboard()) return;
     const auto len = text.size();
     auto mem = GlobalAlloc(GMEM_MOVEABLE, len + 1);
-    memcpy(GlobalLock(mem), text.c_str(), len + 1);
+    memcpy(GlobalLock(mem), text.data(), len + 1);
     GlobalUnlock(mem);
     SetClipboardData(CF_TEXT, mem);
     CloseClipboard();
@@ -50,245 +74,175 @@ ImVec2 toVec2(const CCSize& a) {
     };
 }
 
-CCNode* g_node = nullptr;
+static CCNode* g_selectedNode = nullptr;
+static bool g_reachedSelectedNode = false;
 
-void generateTree(CCNode* node, unsigned int i = 0) {
+void generateTree(CCNode* node, unsigned int index = 0) {
     std::stringstream stream;
-    stream << "[" << i << "] " << getNodeName(node);
+    stream << "[" << index << "] " << getNodeName(node);
     if (node->getTag() != -1)
         stream << " (" << node->getTag() << ")";
     const auto childrenCount = node->getChildrenCount();
     if (childrenCount)
         stream << " {" << childrenCount << "}";
 
-    auto visible = node->isVisible();
-    ImGui::PushStyleColor(ImGuiCol_Text, visible ? ImVec4{ 1.f, 1.f, 1.f, 1.f } : ImVec4{ 0.5f, 0.5f, 0.5f, 1.f });
-
-    bool main = ImGui::TreeNode(node, stream.str().c_str());
-    bool hovered = ImGui::IsItemHovered();
-    bool attributes = main && ImGui::TreeNode(node + 1, "Attributes");
-    hovered |= main && ImGui::IsItemHovered();
-    if ((attributes || hovered) && ImGui::GetIO().KeyShift) {
-        auto& foreground = *ImGui::GetForegroundDrawList();
-        auto parent = node->getParent();
-        auto anchorPoint = node->getAnchorPoint();
-        auto boundingBox = node->boundingBox();
-        auto bbMin = CCPoint(boundingBox.getMinX(), boundingBox.getMinY());
-        auto bbMax = CCPoint(boundingBox.getMaxX(), boundingBox.getMaxY());
-
-        auto cameraParent = node;
-        while (cameraParent) {
-            auto camera = cameraParent->getCamera();
-
-            auto offsetX = 0.f;
-            auto offsetY = 0.f;
-            auto offsetZ = 0.f;
-            camera->getEyeXYZ(&offsetX, &offsetY, &offsetZ);
-            auto offset = CCPoint(offsetX, offsetY);
-            // they don't have -= for some reason
-            bbMin = bbMin - offset;
-            bbMax = bbMax - offset;
-
-            cameraParent = cameraParent->getParent();
-        }
-
-        auto min = toVec2(parent ? parent->convertToWorldSpace(bbMin) : bbMin);
-        auto max = toVec2(parent ? parent->convertToWorldSpace(bbMax) : bbMax);
-        foreground.AddRectFilled(min, max, hovered ? 0x509999FF : 0x50FFFFFF);
+    auto flags = ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_SpanFullWidth;
+    if (g_selectedNode == node) {
+        flags |= ImGuiTreeNodeFlags_Selected;
+        g_reachedSelectedNode = true;
     }
+    if (node->getChildrenCount() == 0)
+        flags |= ImGuiTreeNodeFlags_Leaf;
 
-    if (main) {
-        if (attributes) {
-            if (ImGui::Button("Delete")) {
-                node->removeFromParentAndCleanup(true);
-                ImGui::TreePop();
-                ImGui::TreePop();
-                return;
-            }
-            ImGui::SameLine();
-            if (ImGui::Button("Add Child")) {
-                ImGui::OpenPopup("Add Child");
-            }
-
-            if (ImGui::BeginPopupModal("Add Child")) {
-                static int item = 0;
-                ImGui::Combo("Node", &item, "CCNode\0CCLabelBMFont\0CCLabelTTF\0CCSprite\0CCMenuItemSpriteExtra\0");
-
-                static int tag = -1;
-                ImGui::InputInt("Tag", &tag);
-
-                static char text[256];
-                static char labelFont[256] = "bigFont.fnt";
-                if (item == 1) {
-                    ImGui::InputText("Text", text, 256);
-                    ImGui::InputText("Font", labelFont, 256);
-                }
-                static int fontSize = 20;
-                if (item == 2) {
-                    ImGui::InputText("Text", text, 256);
-                    ImGui::InputInt("Font Size", &fontSize);
-                }
-                static bool frame = false;
-                if (item == 3 || item == 4) {
-                    ImGui::InputText("Texture", text, 256);
-                    ImGui::Checkbox("Frame", &frame);
-                }
-
-                ImGui::Separator();
-
-                if (ImGui::Button("Add")) {
-                    CCNode* _child = nullptr;
-                    switch (item) {
-                    case 0:
-                        _child = CCNode::create();
-                        break;
-                    case 1: {
-                        auto child = CCLabelBMFont::create(text, labelFont);
-                        _child = child;
-                        break;
-                    }
-                    case 2: {
-                        auto child = CCLabelTTF::create(text, "Arial", fontSize);
-                        _child = child;
-                        break;
-                    }
-                    case 3: {
-                        CCSprite* child;
-                        if (frame)
-                            child = CCSprite::createWithSpriteFrameName(text);
-                        else
-                            child = CCSprite::create(text);
-                        _child = child;
-                        break;
-                    }
-                    case 4: {
-                        CCSprite* sprite;
-                        if (frame)
-                            sprite = CCSprite::createWithSpriteFrameName(text);
-                        else
-                            sprite = CCSprite::create(text);
-                        // _child = CCMenuItemSpriteExtra::create(sprite, sprite, nullptr, nullptr);
-                        break;
-                    }
-                    }
-                    if (_child != nullptr) {
-                        _child->setTag(tag);
-                        node->addChild(_child);
-                    }
-
-                    ImGui::CloseCurrentPopup();
-                }
-                ImGui::SameLine();
-                if (ImGui::Button("Cancel")) {
-                    ImGui::CloseCurrentPopup();
-                }
-                ImGui::EndPopup();
-            }
-            ImGui::Text("Addr: 0x%p", node);
-            ImGui::SameLine();
-            if (ImGui::Button("Copy")) {
-                std::stringstream stream;
-                stream << std::uppercase << std::hex << reinterpret_cast<uintptr_t>(node);
-                clipboardText(stream.str());
-            }
-            if (node->getUserData()) {
-                ImGui::Text("User data: 0x%p", node->getUserData());
-            }
-
-            auto pos = node->getPosition();
-            float _pos[2] = { pos.x, pos.y };
-            ImGui::DragFloat2("Position", _pos);
-            node->setPosition({ _pos[0], _pos[1] });
-
-            float _scale[3] = { node->getScale(), node->getScaleX(), node->getScaleY() };
-            ImGui::DragFloat3("Scale", _scale, 0.025);
-            // amazing
-            if (node->getScale() != _scale[0])
-                node->setScale(_scale[0]);
-            else {
-                node->setScaleX(_scale[1]);
-                node->setScaleY(_scale[2]);
-            }
-
-            float _rot[3] = { node->getRotation(), node->getRotationX(), node->getRotationY() };
-            ImGui::DragFloat3("Rotation", _rot);
-            if (node->getRotation() != _rot[0])
-                node->setRotation(_rot[0]);
-            else {
-                node->setRotationX(_rot[1]);
-                node->setRotationY(_rot[2]);
-            }
-
-            float _skew[2] = { node->getSkewX(), node->getSkewY() };
-            ImGui::DragFloat2("Skew", _skew);
-            node->setSkewX(_skew[0]);
-            node->setSkewY(_skew[1]);
-
-            auto anchor = node->getAnchorPoint();
-            ImGui::DragFloat2("Anchor Point", &anchor.x, 0.05f, 0.f, 1.f);
-            node->setAnchorPoint(anchor);
-
-            auto contentSize = node->getContentSize();
-            ImGui::DragFloat2("Content Size", &contentSize.width);
-            if (contentSize != node->getContentSize())
-                node->setContentSize(contentSize);
-
-            int zOrder = node->getZOrder();
-            ImGui::InputInt("Z", &zOrder);
-            if (node->getZOrder() != zOrder)
-                node->setZOrder(zOrder);
-            
-            ImGui::Checkbox("Visible", &visible);
-            if (visible != node->isVisible())
-                node->setVisible(visible);
-
-            if (dynamic_cast<CCRGBAProtocol*>(node) != nullptr) {
-                auto rgbaNode = dynamic_cast<CCRGBAProtocol*>(node);
-                auto color = rgbaNode->getColor();
-                float _color[4] = { color.r / 255.f, color.g / 255.f, color.b / 255.f, rgbaNode->getOpacity() / 255.f };
-                ImGui::ColorEdit4("Color", _color);
-                rgbaNode->setColor({
-                    static_cast<GLubyte>(_color[0] * 255),
-                    static_cast<GLubyte>(_color[1] * 255),
-                    static_cast<GLubyte>(_color[2] * 255)
-                });
-                rgbaNode->setOpacity(_color[3] * 255);
-            }
-            if (dynamic_cast<CCLabelProtocol*>(node) != nullptr) {
-                auto labelNode = dynamic_cast<CCLabelProtocol*>(node);
-                auto labelStr = labelNode->getString();
-                char text[256];
-                strcpy_s(text, labelStr);
-                ImGui::InputText("Text", text, 256);
-                if (strcmp(text, labelStr)) {
-                    labelNode->setString(text);
-                }
-            }
-
-            ImGui::TreePop();
-        }
-        
+    ImGui::PushStyleColor(ImGuiCol_Text, node->isVisible() ? ImVec4 { 1.f, 1.f, 1.f, 1.f } : ImVec4 { 0.8f, 0.8f, 0.8f, 1.f });
+    const bool isOpen = ImGui::TreeNodeEx(node, flags, stream.str().c_str());
+    if (ImGui::IsItemClicked()) {
+        g_selectedNode = node;
+        g_reachedSelectedNode = true;
+    }
+    if (isOpen) {
         auto children = node->getChildren();
-        for (unsigned int i = 0; i < node->getChildrenCount(); ++i) {
-            auto child = children->objectAtIndex(i);
-            generateTree(dynamic_cast<CCNode*>(child), i);
+        if (children) {
+            for (unsigned int i = 0; i < children->count(); ++i) {
+                auto child = children->objectAtIndex(i);
+                generateTree(static_cast<CCNode*>(child), i);
+            }
         }
         ImGui::TreePop();
     }
     ImGui::PopStyleColor();
 }
 
+#define CONCAT_(a, b) a ## b
+#define CONCAT(a, b) CONCAT_(a, b)
+
+void renderNodeProperties(CCNode* node) {
+    if (ImGui::Button("Delete")) {
+        node->removeFromParentAndCleanup(true);
+        return;
+    }
+    ImGui::Text("Addr: 0x%p", node);
+    ImGui::SameLine();
+    if (ImGui::Button("Copy")) {
+        std::stringstream stream;
+        stream << std::uppercase << std::hex << reinterpret_cast<uintptr_t>(node);
+        setClipboardText(stream.str());
+    }
+    if (node->getUserData())
+        ImGui::Text("User data: 0x%p", node->getUserData());
+
+    const auto menuItemNode = dynamic_cast<CCMenuItem*>(node);
+    if (menuItemNode) {
+        const auto selector = public_cast(menuItemNode, m_pfnSelector);
+        ImGui::Text("CCMenuItem selector: %p", union_cast<void*>(selector));
+    }
+
+#define GET_SET_FLOAT2(name, label) { \
+    auto point = node->get##name(); \
+    ImGui::DragFloat2(label, reinterpret_cast<float*>(&point)); \
+    if (point != node->get##name()) \
+        node->set##name(point); \
+}
+
+#define GET_SET_INT(name, label) { \
+    auto value = node->get##name(); \
+    ImGui::InputInt(label, &value); \
+    if (value != node->get##name()) \
+        node->set##name(value); \
+}
+
+#define GET_SET_CHECKBOX(name, label) { \
+    auto value = node->is##name(); \
+    ImGui::Checkbox(label, &value); \
+    if (value != node->is##name()) \
+        node->set##name(value); \
+}
+
+    GET_SET_FLOAT2(Position, "Position");
+
+#define dragXYBothIDK(name, label, speed) { \
+    float values[3] = { node->get##name(), node->CONCAT(get, CONCAT(name, X))(), node->CONCAT(get, CONCAT(name, Y))() }; \
+    if (ImGui::DragFloat3(label, values, speed)) { \
+        if (node->get##name() != values[0]) \
+            node->set##name(values[0]); \
+        else { \
+            node->CONCAT(set, CONCAT(name, X))(values[1]); \
+            node->CONCAT(set, CONCAT(name, Y))(values[2]); \
+        } \
+    } \
+}
+    dragXYBothIDK(Scale, "Scale", 0.025f);
+    dragXYBothIDK(Rotation, "Rotation", 1.0f);
+
+#undef dragXYBothIDK
+
+    {
+        auto anchor = node->getAnchorPoint();
+        ImGui::DragFloat2("Anchor Point", &anchor.x, 0.05f, 0.f, 1.f);
+        node->setAnchorPoint(anchor);
+    }
+
+    GET_SET_FLOAT2(ContentSize, "Content Size");
+    GET_SET_INT(ZOrder, "Z Order");
+    GET_SET_CHECKBOX(Visible, "Visible");
+
+    const auto rgbaNode = dynamic_cast<CCNodeRGBA*>(node);
+    if (rgbaNode) {
+        auto color = rgbaNode->getColor();
+        float colorValues[4] = {
+            color.r / 255.f,
+            color.g / 255.f,
+            color.b / 255.f,
+            rgbaNode->getOpacity() / 255.f
+        };
+        if (ImGui::ColorEdit4("Color", colorValues)) {
+            rgbaNode->setColor({
+                static_cast<GLubyte>(colorValues[0] * 255),
+                static_cast<GLubyte>(colorValues[1] * 255),
+                static_cast<GLubyte>(colorValues[2] * 255)
+            });
+            rgbaNode->setOpacity(static_cast<GLubyte>(colorValues[3] * 255.f));
+        }
+    }
+    const auto labelNode = dynamic_cast<CCLabelProtocol*>(node);
+    if (labelNode) {
+        std::string str = labelNode->getString();
+        if (ImGui::InputTextMultiline("Text", &str))
+            labelNode->setString(str.c_str());
+    }
+}
+
 bool g_showWindow = false;
+static ImFont* g_font = nullptr;
 
 void draw() {
+    if (g_font) ImGui::PushFont(g_font);
     if (g_showWindow) {
-        auto& style = ImGui::GetStyle();
-        style.ColorButtonPosition = ImGuiDir_Left;
-
-        auto director = CCDirector::sharedDirector();
         if (ImGui::Begin("cocos2d explorer", nullptr, ImGuiWindowFlags_HorizontalScrollbar)) {
-            auto curScene = director->getRunningScene();
-            generateTree(curScene);
+
+            const auto avail = ImGui::GetContentRegionAvail();
+
+            ImGui::BeginChild("explorer.tree", ImVec2(avail.x * 0.5f, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+            auto node = CCDirector::sharedDirector()->getRunningScene();
+            g_reachedSelectedNode = false;
+            generateTree(node);
+            
+            ImGui::EndChild();
+
+            if (!g_reachedSelectedNode)
+                g_selectedNode = nullptr;
+
+            ImGui::SameLine();
+
+            ImGui::BeginChild("explorer.options");
+
+            if (g_selectedNode)
+                renderNodeProperties(g_selectedNode);
+            else
+                ImGui::Text("Select a node to edit its properties :-)");
+
+            ImGui::EndChild();
         }
         ImGui::End();
     }
@@ -301,12 +255,16 @@ void draw() {
         ImGui::Text("Cocos explorer loaded, press K to toggle");
         ImGui::End();
     }
+    if (g_font) ImGui::PopFont();
 }
 
 void init() {
     auto& style = ImGui::GetStyle();
 	style.WindowTitleAlign = ImVec2(0.5f, 0.5f);
 	style.WindowBorderSize = 0;
+    style.ColorButtonPosition = ImGuiDir_Left;
+
+    g_font = ImGui::GetIO().Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\consola.ttf", 14.f);
 
 	auto colors = style.Colors;
 	colors[ImGuiCol_FrameBg] = ImVec4(0.31f, 0.31f, 0.31f, 0.54f);
